d9a0f175cd1a5ea2f808783e6e545e7a47f7b7cc
indivar007 <kumarindivar85@gmail.com>
2026-03-07 12:34:39 +0530

    learned if else

---
+++ 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b ;
+    cin>>a >>b;
+    if(a>b){
+        cout<<a<<" is greater than "<<b;
+    }
+    else if(a<b){
+        cout<<a<<" is less than "<<b;
+    }
+    else{
+        cout<<a<<" is equal to "<<b;
+    }
+
+
+}
+    
