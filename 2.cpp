7dc5241cbc8b9ae22169e1470debb7f4d3171104
indivar007 <kumarindivar85@gmail.com>
2026-03-07 13:45:40 +0530

    infinite loop

--- 2.cpp
+++ 2.cpp
@@ -2,15 +2,13 @@
 using namespace std;
 
 int main(){
-    int i;
-    for (i= 10;i >=1; i--){
-        for(int j = 1; j<=i; j++){
-            cout<<"*";
-
+    int i=0;
+    for(; ;){
+        if(i<5){
+            cout<<i;
+            i=i+1;
         }
-          cout<<endl;
-       
-}
+    }
 return 0;
 }
 
