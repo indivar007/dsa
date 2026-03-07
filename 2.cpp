f5bb32ba6c2cb29540387ef989cf565ad0053aa1
indivar007 <kumarindivar85@gmail.com>
2026-03-07 14:01:39 +0530

    learning for loops

--- 2.cpp
+++ 2.cpp
@@ -2,13 +2,22 @@
 using namespace std;
 
 int main(){
-    int i=0;
-    for(; ;){
-        if(i<5){
-            cout<<i;
-            i=i+1;
+   for(int i=0 ;i<=10 ;i=i+1 ){
+        cout<<"indivar"<<endl;
+   }
+   for(int i = 0 ; i <= 100 ;i= i+1){
+        if (i%2 == 0){
+            cout<<i<<" is even "<<endl;
         }
+        }
+   for(int i=19 ; i <= 200; i= i+1){
+        if(i%19==0){
+            cout<<i<<endl;
+        }
+
     }
+    
+   
 return 0;
 }
 
