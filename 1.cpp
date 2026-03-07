081b18fd8127d6c3b4d30ed406f0e1512b1b442c
indivar007 <kumarindivar85@gmail.com>
2026-03-07 13:22:05 +0530

    learning loops

--- 1.cpp
+++ 1.cpp
@@ -4,16 +4,19 @@ using namespace std;
 int main(){
     int a,b ;
     cin>>a >>b;
+    cout<<"the 2 no are "<<a <<"and"<<b<<endl;
     if(a>b){
-        cout<<a<<" is greater than "<<b;
+        cout<<a<<" is greater than "<<b <<endl;
     }
     else if(a<b){
-        cout<<a<<" is less than "<<b;
+        cout<<a<<" is less than "<<b << endl;
     }
     else{
-        cout<<a<<" is equal to "<<b;
+        cout<<a<<" is equal to "<<b << endl;
+
+        
     }
 
-
+return 0;
 }
     
