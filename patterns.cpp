f09b77e6773c5764d4cb69ef985c2ba7303e3f04
indivar007 <kumarindivar85@gmail.com>
2026-03-08 13:50:18 +0530

    learned to print half diamond

--- patterns.cpp
+++ patterns.cpp
@@ -2,17 +2,42 @@
 using namespace std;
 
 int main(){
+    //this is for pyramid pattern
 
-    for(int i = 0 ; i <= 5; i++){
-    for(int j =0 ; j <=5-i-1; j++){
-        cout<<" ";
-        }
-        for(int k = 0 ; k <= i; k++){
-            cout<<"* ";
+//     for(int i = 0 ; i <= 5; i++){
+//     for(int j =0 ; j <=i; j++){
+//         cout<<" ";
+//         }
+//         for(int k = 0 ; k <= 5-i; k++){
+//             cout<<"* ";
         
 
+//     }
+//    cout<<endl;
+// }
+int n;
+cin>>n;
+
+for(int i = 0 ;i<= n ;i++){
+   
+    for(int k = 0 ; k <= i; k++){
+        cout<<"* ";
     }
-   cout<<endl;
+     
+
+    cout<<endl;
+
 }
+for(int i = 0 ;i<= n ;i++){
+   
+    for(int k = 0 ; k <= n-i-1; k++){
+        cout<<"* ";
+    }
+     
+    
+    cout<<endl;
+
+}
+
  return 0;
 }
