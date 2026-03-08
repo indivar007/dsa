a92751c3ea7e88b5007950a178406c7dcdf60fa4
indivar007 <kumarindivar85@gmail.com>
2026-03-08 18:39:33 +0530

    increment and decrement operaror

--- patterns.cpp
+++ patterns.cpp
@@ -2,42 +2,27 @@
 using namespace std;
 
 int main(){
-    //this is for pyramid pattern
-
-//     for(int i = 0 ; i <= 5; i++){
-//     for(int j =0 ; j <=i; j++){
-//         cout<<" ";
-//         }
-//         for(int k = 0 ; k <= 5-i; k++){
-//             cout<<"* ";
+    int n;
+    cin>>n;
+    //upper half
+    for(int i = 0 ; i<n ; i++){
+        for(int k = 0 ; k< n-i-1;k++){
+            cout<<" ";
+        }
+        for(int j = 0 ; j<=i ;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+        }
+        for(int i = n-2 ; i>=0 ; i--){
+            for(int k = 0 ; k< n-i-1;k++){
+            cout<<" ";
+        }
         
-
-//     }
-//    cout<<endl;
-// }
-int n;
-cin>>n;
-
-for(int i = 0 ;i<= n ;i++){
-   
-    for(int k = 0 ; k <= i; k++){
-        cout<<"* ";
-    }
-     
-
-    cout<<endl;
-
-}
-for(int i = 0 ;i<= n ;i++){
-   
-    for(int k = 0 ; k <= n-i-1; k++){
-        cout<<"* ";
-    }
-     
-    
-    cout<<endl;
-
-}
-
+        for(int j = 0 ; j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+        }
  return 0;
 }
