f1745e6aa0d4419b6037d2b1f555f702b8686f4f
indivar007 <kumarindivar85@gmail.com>
2026-03-10 00:38:15 +0530

    function class work

--- function.cpp
+++ function.cpp
@@ -7,25 +7,72 @@ using namespace std ;
 //     int  sum = a+b+c;
 //     return sum;//using return function
 // }
-int maxOf(){
+// int maxOf(){
 
-    int a, b, c;
-    cin>>a>>b>>c;
-    if(a>b && a > c){
-        cout<<"the largest no is:"<<a<<endl;
+//     int a, b, c;
+//     cin>>a>>b>>c;
+//     if(a>b && a > c){
+//         cout<<"the largest no is:"<<a<<endl;
+//     }
+//     else if(b>a && b >c){
+//         cout<<"the largest no is :"<<b<<endl;
+//     }
+//     else{
+//         cout<<"the largest no is:"<<c<<endl;
+//     }
+
+// }
+int count(int n)
+{
+    for(int i = 1; i <=n ;i++){
+        cout<<i<<endl;
     }
-    else if(b>a && b >c){
-        cout<<"the largest no is :"<<b<<endl;
+}
+int checkPrime(int a){
+    int i ;
+    for(i =2 ; i < a;i++){
+        if(a % i == 0){
+            cout<<"its composite number";
+            return 0;
+
+        }
+             cout<<"prime"<<endl;
+          
+    }
+}
+void evenOdd(int n){
+
+    if(n % 2 == 0){
+        cout<<"even";
     }
     else{
-        cout<<"the largest no is:"<<c<<endl;
+        cout<<"odd";
     }
+
+}
+
+int addN(int n){
+    int sum = 0;
+    for(int i =0 ; i <= n; i++ ){
+        sum = sum+i;
+        
+    }
+    return sum;
 }
 
 int main(){
-     int max;
+    
 
     // cout<<  (sum = printSum( 5 ,4 ,6));
 
-    maxOf();
+    // maxOf();/
+    count(100);
+    checkPrime(9);
+    int a;
+    
+    cin>>a;
+    evenOdd(a);
+    addN(a);
+
+    return 0;
 }
