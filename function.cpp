1316f7d02139d927014ed27e5918b52030b932ee
indivar007 <kumarindivar85@gmail.com>
2026-03-09 23:33:41 +0530

    function adding of 3 no

---
+++ function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ;
+
+int printSum(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    int  sum = a+b+c;
+    cout<<sum<<endl;
+}
+
+int main(){
+
+    printSum();
+
+}
