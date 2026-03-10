dd5a418f8946f6cd193e358be9dc80bfc259732a
indivar007 <kumarindivar85@gmail.com>
2026-03-11 00:17:59 +0530

    assignment question

--- assesment1.cpp
+++ assesment1.cpp
@@ -2,32 +2,47 @@
 using namespace std;
 
 //area of circle
-float areaOfCircle(int r){
-    float pi = 3.14;
-    float area = pi*r*r;
-    return area;
+// float areaOfCircle(int r){
+//     float pi = 3.14;
+//     float area = pi*r*r;
+//     return area;
 
-}
-//factorial of a no
-float factorial(int n){
-    int fact=1;
-     for(int i = 1 ; i <= n ; i++){
-        fact= fact*i;
+// }
+// //factorial of a no
+// float factorial(int n){
+//     int fact=1;
+//      for(int i = 1 ; i <= n ; i++){
+//         fact= fact*i;
 
+//      }
+//      return fact;
+// }
+//create number using digit
+
+int digit(int n){
+     int number = 0 ; 
+     int digit;
+
+     for(int i =0 ; i < n ; i++ ){
+        cin>>digit;
+        number = number * 10 +digit;
      }
-     return fact;
+     return number;
+
 }
 
 int main(){
    int n;
    cin>>n;
-   float result = areaOfCircle(n);
-   cout<<result<<endl;
+//    float result = areaOfCircle(n);
+//    cout<<result<<endl;
 
-   int fact = factorial(n);
-   cout<<fact<<endl;
+//    int fact = factorial(n);
+//    cout<<fact<<endl;
 
-
+   
+    int number = digit(n);
+    cout<<number<<endl;
 
 
 
