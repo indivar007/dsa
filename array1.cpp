c84a00ac4348905b3ac3a19b7f7bd44675323853
indivar007 <kumarindivar85@gmail.com>
2026-03-11 17:28:58 +0530

    printing an array and linear serach using function

--- array1.cpp
+++ array1.cpp
@@ -1,30 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-   //linear search in array
-   int arr[5];
-   int n =5;
-   int target = 781;
-   bool flag = 0; 
-   for(int i =0 ; i<n ;i++){
-    cin>>arr[i];
-
-
-   }
-   for(int i =0 ; i<n ;i++){
-    if(arr[i] == target){
-        flag = 1;
-        break;
+void printArray(int arr[], int size){
+    for(int i = 0 ; i<size;i++){
+        cin>>arr[i];
+    }
+    for(int i = 0 ; i<size;i++){
+       cout<<arr[i]<<endl;
     }
     
-   }
-   if(flag == 1){
-    cout<<"found"<<endl;
-   }
-   else{
-    cout<<"not found";
-   }
+}
+int linearSearch(int arr[],int size,int target){
+    for(int i = 0 ; i< size ; i++){
+        if(arr[i] == target){
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(){
+    int arr[5];
+    int size = 5;
+    int target = 100;
+    
+    printArray(arr,size) ;
+    int result  = linearSearch(arr, size, target);
+    if(result == 1 ){
+        cout<<"found"<<endl;
+    }
+    else{
+        cout<<"not found"<<endl;
+    }
+
+
+
+  
     return 0;
 
 }
