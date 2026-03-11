bb5b07863680c6b23e593993e73319e5f5920040
indivar007 <kumarindivar85@gmail.com>
2026-03-11 16:45:32 +0530

    making an array and doubling it

--- array1.cpp
+++ array1.cpp
@@ -2,25 +2,37 @@
 using namespace std;
 
 int main(){
-    int arr[50]= {1,2,3,4,5,6,7,8,9,10,111,12,12,1,2,1,2,1,2,1,2,1};
+    // int arr[50]= {1,2,3,4,5,6,7,8,9,10,111,12,12,1,2,1,2,1,2,1,2,1};
     
-    cout<<"the adress of array is"<<
-    int(arr)<<endl;
-    cout<< arr[2];
-    int n  =50;
-    for(int i =0 ;i<=n;i++){
-        cout<<i<<" : "<<arr[i]<<endl;
-    }
+    // cout<<"the adress of array is"<<
+    // int(arr)<<endl;
+    // cout<< arr[2];
+    // int n  =50;
+    // for(int i =0 ;i<=n;i++){
+    //     cout<<i<<" : "<<arr[i]<<endl;
+    // }
 
-    int age[10];
-    int a = 10 ;
-    for(int i  = 0 ; i < a ;i++ ){
-        cout<<"the index is"<< i<<":";
-        cin>>age[i];
+    // int age[10];
+    // int a = 10 ;
+    // for(int i  = 0 ; i < a ;i++ ){
+    //     cout<<"the index is"<< i<<":";
+    //     cin>>age[i];
         
+    // }
+    // for(int i = 0 ;i <a ;i++){
+    //     cout<<age[i]<<endl;
+    // }
+
+
+    int arr[10];
+    int n =10;
+    for(int i = 0;i<n ;i++){
+        cin>>arr[i];
+        arr[i] = arr[i]*2;
     }
-    for(int i = 0 ;i <a ;i++){
-        cout<<age[i]<<endl;
+    for(int i = 0; i<n ; i++){
+        cout<<arr[i]<<endl;
     }
     return 0;
+
 }
