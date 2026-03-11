4042c692dedf2d9486cc176a1e3cc4b2306b894a
indivar007 <kumarindivar85@gmail.com>
2026-03-11 17:08:48 +0530

    linear searching of array

--- array1.cpp
+++ array1.cpp
@@ -2,40 +2,29 @@
 using namespace std;
 
 int main(){
-    // int arr[50]= {1,2,3,4,5,6,7,8,9,10,111,12,12,1,2,1,2,1,2,1,2,1};
-    
-    // cout<<"the adress of array is"<<
-    // int(arr)<<endl;
-    // cout<< arr[2];
-    // int n  =50;
-    // for(int i =0 ;i<=n;i++){
-    //     cout<<i<<" : "<<arr[i]<<endl;
-    // }
-
-    // int age[10];
-    // int a = 10 ;
-    // for(int i  = 0 ; i < a ;i++ ){
-    //     cout<<"the index is"<< i<<":";
-    //     cin>>age[i];
-        
-    // }
-    // for(int i = 0 ;i <a ;i++){
-    //     cout<<age[i]<<endl;
-    // }
+   //linear search in array
+   int arr[5];
+   int n =5;
+   int target = 781;
+   bool flag = 0; 
+   for(int i =0 ; i<n ;i++){
+    cin>>arr[i];
 
 
-    
-
-    int arr[5]= {10,25,300,990,878};
-    int n = 5;
-    int sum = 0;
-    for(int i = 0; i<n ;i++){
-        sum = sum +arr[i];
-        cout<<sum<<endl;
-
+   }
+   for(int i =0 ; i<n ;i++){
+    if(arr[i] == target){
+        flag = 1;
+        break;
     }
-    cout<<"the sum is : "<<sum<<endl;
-
+    
+   }
+   if(flag == 1){
+    cout<<"found"<<endl;
+   }
+   else{
+    cout<<"not found";
+   }
     return 0;
 
 }
