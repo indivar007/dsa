d683958366a234716879e6b1631f018770da5df9
indivar007 <kumarindivar85@gmail.com>
2026-03-14 15:05:12 +0530

    sort 0s and ones in an array

--- array2.cpp
+++ array2.cpp
@@ -2,25 +2,67 @@
 using namespace std;
 
 
-void triplets(int arr[], int size){
-    int i , j ,k;
-    for(i = 0 ; i<size; i++){
-        for(j = i+1 ; j<size ; j++){
-            for(k = j+1 ; k<size; k++){
-                cout<<arr[i]<<','<<arr[j]<<','<<arr[k]<<endl;
-            }
+void sort(int arr[], int size){
+    int countzero =0 ;
+    int onecount =0;
+
+    int i,j;
+
+    for (i = 0 ; i< size ;i++){
+        if(arr[i] == 0 ){
+            countzero++;
+            
+        
+        }
+        if(arr[i]== 1){
+            onecount++;
         }
     }
+    cout<<countzero<<endl;
+    cout<<onecount<<endl;
+
+
+
+    // for(i = 0;i < countzero ; i++){
+    //     arr[i] = 0;
+        
+    // }
+    // for(j =  i ; j<size; j++){
+    //     arr[j]= 1;
+        
+    // }
+    int index =0;
+    while(countzero--){
+        arr[index] = 0;
+        index++;
+        
+    }
+
+    while(onecount--){
+        arr[index] =1;
+        index++;
+
+    }
+
+    
+
+   
 }
    
 
 
 int main(){
-    //taking an array and find unique element in it
+    //taking an array and sorting 0 and 1s
+ int arr[]={0,1,1,1,0,0,0,0,0,0,1,1,1,0,0};
+ int size =14;
+ sort(arr,size);
 
-    int arr[5]={0,1,2,3,4};
-    int size =5  ;
-    triplets(arr,5);
+
+  for (int i = 0 ; i< size ;i++){
+    cout<<arr[i];
+  }
+
+   
     
     
 
