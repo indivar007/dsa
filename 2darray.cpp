199e7183e6eb88e60e53fefca05834c9e4da5187
indivar007 <kumarindivar85@gmail.com>
2026-03-14 16:40:21 +0530

    searching element in an array

--- 2darray.cpp
+++ 2darray.cpp
@@ -1,26 +1,50 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int arr[3][4];
-    for(int i = 0 ; i<3 ; i++){
-        for(int j = 0; j<4 ; j++){
+void array2d(int arr[][4], int rows,int cols){
+    int target =70;
+    bool flag =0;
+    for(int i = 0 ; i<rows ; i++){
+        for(int j = 0; j<cols ; j++){
             cin>>arr[i][j];
+
         }
 
     }
     for(int i = 0 ; i<3 ; i++){
         for(int j = 0; j<4 ; j++){
-            cout<<arr[i][j]<<" ";
-        }
+            if(arr[i][j] == target ){
+                flag =1;
 
+                
+
+            }
+            
+           
+        }
+        
+
+    }
+    if(flag ==1 ){
+        cout<<"target found";
+
+    }
+    else{
+        cout<<"target not found";
     }
     cout<<endl;
 
-    cout<<arr[2][3]<<endl;
+  
 
+}
+
+int main(){
     
+    int arr[3][4];
+    int rows;
+    int column;
 
+    array2d(arr, 3,4);
 
     return 0;
 }
