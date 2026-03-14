d9d229b5655803801820a9bb82fe415e092bf5fb
indivar007 <kumarindivar85@gmail.com>
2026-03-14 14:24:13 +0530

    triplets i  arrahy

--- array2.cpp
+++ array2.cpp
@@ -2,32 +2,25 @@
 using namespace std;
 
 
-int pairing(int arr[],int size){
-    
-    for(int i =0 ; i< size;i++){
-        for(int j = 0;j <size;j++){
-
-            if(arr[i] == arr[j]){
-                cout<<endl;
-
-            }else{
-            cout<<arr[i]<<","<<arr[j]<<endl  ;    
-        
+void triplets(int arr[], int size){
+    int i , j ,k;
+    for(i = 0 ; i<size; i++){
+        for(j = i+1 ; j<size ; j++){
+            for(k = j+1 ; k<size; k++){
+                cout<<arr[i]<<','<<arr[j]<<','<<arr[k]<<endl;
             }
-
+        }
     }
-
-}}
+}
    
 
 
 int main(){
     //taking an array and find unique element in it
 
-
-    int arr[9]= {10,20,30,40,50,40,30,20,10};
-    int size = 9;
-     pairing(arr,9);
+    int arr[5]={0,1,2,3,4};
+    int size =5  ;
+    triplets(arr,5);
     
     
 
