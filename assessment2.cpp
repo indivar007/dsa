c6a1130e8f00606b7c536a80e66e1adee730ef93
indivar007 <kumarindivar85@gmail.com>
2026-03-15 19:23:06 +0530

    flating 2d array into 1d then pairing

--- assessment2.cpp
+++ assessment2.cpp
@@ -11,25 +11,38 @@ void allPair(vector<vector<int>>&arr,int rows ,int cols){
         }
 
     }
-    for(int i = 0 ; i < rows ; i++){
-        for(int j = 0 ; j < cols ; j++){
+    // for(int i = 0 ; i < rows ; i++){
+    //     for(int j = 0 ; j < cols ; j++){
 
-            for(int k = i ; k <rows ; k++){
+    //         for(int k = i ; k <rows ; k++){
 
-                for(int l = j+1 ; l < cols ; l++ ){
-                    cout<<"("<<arr[i][j]<<" ,"<<arr[k][l]<<"\t"<<" )";
+    //             for(int l = j+1 ; l < cols ; l++ ){
+    //                 cout<<"("<<arr[i][j]<<" ,"<<arr[k][l]<<"\t"<<" )";
 
-                }
+    //             }
 
-            }
+    //         }
            
-            cout<<endl;
-        }
+    //         cout<<endl;
+    //     }
 
         
 
+    // }
+  //how to make a vectoe flat 
+  vector<int> flat;
+  for(int i = 0 ; i <rows;i++){
+    for(int j = 0 ;  j < cols ; j++){
+        flat.push_back(arr[i][j]);
+        cout<<arr[i][j];
+    }
+  }
+  for(int i = 0 ; i <flat.size() ; i++){
+    for(int j = i+1 ; j<flat.size(); j++){
+        cout<<"("<<flat[i]<<","<<flat[j]<<")"<<endl;;
     }
 
+  }
 }
 
 int main(){
