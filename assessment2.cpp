0fe3ca5612b918dbf3401ff4f57eb96df4cf653a
indivar007 <kumarindivar85@gmail.com>
2026-03-17 21:46:06 +0530

    finding missing no

--- assessment2.cpp
+++ assessment2.cpp
@@ -2,56 +2,34 @@
 #include <vector>
 using namespace std;
 
-//finding key pair in a 2d array
-void allPair(vector<vector<int>>&arr,int rows ,int cols){
-    for(int i = 0 ; i < rows ; i++){
-        for(int j = 0 ; j < cols ; j++){
-            cin>>arr[i][j];
 
-        }
+//missing number
+void missingNumber(vector<int> &arr,int n) {
+    for(int i =0;i<arr.size();i++){
+        cin>>arr[i];
 
-    }
-    // for(int i = 0 ; i < rows ; i++){
-    //     for(int j = 0 ; j < cols ; j++){
-
-    //         for(int k = i ; k <rows ; k++){
-
-    //             for(int l = j+1 ; l < cols ; l++ ){
-    //                 cout<<"("<<arr[i][j]<<" ,"<<arr[k][l]<<"\t"<<" )";
-
-    //             }
-
-    //         }
-           
-    //         cout<<endl;
-    //     }
-
-        
-
-    // }
-  //how to make a vectoe flat 
-  vector<int> flat;
-  for(int i = 0 ; i <rows;i++){
-    for(int j = 0 ;  j < cols ; j++){
-        flat.push_back(arr[i][j]);
-        cout<<arr[i][j];
-    }
-  }
-  for(int i = 0 ; i <flat.size() ; i++){
-    for(int j = i+1 ; j<flat.size(); j++){
-        cout<<"("<<flat[i]<<","<<flat[j]<<")"<<endl;;
-    }
-
-  }
 }
 
+    int expectedSum = n * (n+1)/2;
+    int sum = 0;
+    for(int i = 0 ; i < arr.size();i++){
+        sum += arr[i];
+    }
+
+    int missing = expectedSum-sum;
+    cout<<missing;
+
+}
+
+
+
+
 int main(){
-    int rows = 4;
-    int cols = 4;
-    vector<vector<int>> arr(rows, vector<int>(cols));
+     int size=5;
+   vector<int> arr(size);
+ 
+   missingNumber(arr,size);
+
    
-
-
-    allPair(arr,rows,cols);
 
 }
