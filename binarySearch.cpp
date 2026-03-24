608abdcaa583f4f3ce2eeccfcc9bc15d53c12b42
indivar007 <kumarindivar85@gmail.com>
2026-03-24 21:39:19 +0530

    find missing no using binary search

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -3,59 +3,82 @@
 #include <algorithm>
 using namespace std;
 
-int firstOccurance(vector<int> &arr,int size, int target){
-    int left = 0 ;
-    int right = size-1;
-    int ans =-1;
+// int firstOccurance(vector<int> &arr,int size, int target){
+//     int left = 0 ;
+//     int right = size-1;
+//     int ans =-1;
     
-    while(left <= right ){
-        int mid = (left+right)/2;
-        if(arr[mid] == target){
-            ans = mid;
-            right = mid-1;
+//     while(left <= right ){
+//         int mid = (left+right)/2;
+//         if(arr[mid] == target){
+//             ans = mid;
+//             right = mid-1;
 
-        }
-        else if(arr[mid] > target){
-            right = mid -1;
+//         }
+//         else if(arr[mid] > target){
+//             right = mid -1;
 
-        }
-        else{
-            left = mid +1;
-        }
-    }
-    return ans;
+//         }
+//         else{
+//             left = mid +1;
+//         }
+//     }
+//     return ans;
 
-}
+// }
 
-int lastOcuurance(vector<int> &arr,int size ,int target){
-    int left = 0 ;
+// int lastOcuurance(vector<int> &arr,int size ,int target){
+//     int left = 0 ;
+//     int right = size-1;
+//     int ans;
+//     while(left <= right){
+//         int mid = (left + (right-left)/2);
+//         if(arr[mid] == target){
+//             ans = mid;
+//             left = mid+1;
+
+//         }
+//         else if(arr[mid] < target){
+//             left = mid +1;
+//         }
+//         else{
+//             right = mid-1;
+//         }
+
+//     }
+//     return ans;
+
+// }
+// int totalOcuurance(vector<int> &arr,int size,int target){
+//     int last = lastOcuurance(arr ,size ,target);
+//     int first = firstOccurance(arr,size,target);
+//     int total = last-first+1;
+//     return total;
+
+
+// }
+
+
+int missingNo(vector<int> &arr,int size){
+    int left = 0;
     int right = size-1;
-    int ans;
-    while(left <= right){
-        int mid = (left + (right-left)/2);
-        if(arr[mid] == target){
-            ans = mid;
-            left = mid+1;
+    int ans = size+1;
+    
 
+    while(left <= right){
+        int mid = left + (right-left)/2;
+        if(arr[mid] == mid+1){
+            left = mid+1;
         }
-        else if(arr[mid] < target){
-            left = mid +1;
-        }
-        else{
+        else {
+            ans = mid+1;
             right = mid-1;
         }
 
+
     }
+
     return ans;
-
-}
-int totalOcuurance(vector<int> &arr,int size,int target){
-    int last = lastOcuurance(arr ,size ,target);
-    int first = firstOccurance(arr,size,target);
-    int total = last-first+1;
-    return total;
-
-
 }
 
 int main(){
@@ -66,18 +89,21 @@ int main(){
     for(int i = 0 ; i < size; i++){
         cin>>arr[i];
     }
-    sort(arr.begin(), arr.end());
+    // sort(arr.begin(), arr.end());
 
-    int target ;
-    cin>>target;
+    // int target ;
+    // cin>>target;
 
-    int res = firstOccurance(arr,size,target);
-    cout<<"the index at which target is first ocuured"<<" "<<res<<endl;
+    // int res = firstOccurance(arr,size,target);
+    // cout<<"the index at which target is first ocuured"<<" "<<res<<endl;
 
-    int res1 = lastOcuurance(arr,size,target);
-    cout<<"the index where last ocuurance of target is there at"<<" "<<res1<<endl;
+    // int res1 = lastOcuurance(arr,size,target);
+    // cout<<"the index where last ocuurance of target is there at"<<" "<<res1<<endl;
 
-    int total = totalOcuurance(arr, size,target);
-    cout<<"total no of occurance is"<<" "<<total<<endl;
+    // int total = totalOcuurance(arr, size,target);
+    // cout<<"total no of occurance is"<<" "<<total<<endl;
+
+    int missing = missingNo(arr,size);
+    cout<<missing<<endl;
 
 }
